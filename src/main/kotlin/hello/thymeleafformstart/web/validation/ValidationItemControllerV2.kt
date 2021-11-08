c2e7ebcd8a11dabe5a879250ed5593e41c00d92f
Seokhyun Bae <bus1029@gmail.com>
2021-11-08 20:13:04 +0900

    feat: FieldError, ObjectError를 사용한 사용자 입력값 유지
    - 기존 오류 처리에선 사용자가 입력한 값이 필드에 유지가 안되었지만 FieldError를 사용해서
    사용자가 입력한 값을 그대로 화면으로 반환할 수 있음

--- src/main/kotlin/hello/thymeleafformstart/web/validation/ValidationItemControllerV2.kt
+++ src/main/kotlin/hello/thymeleafformstart/web/validation/ValidationItemControllerV2.kt
@@ -40,9 +40,27 @@ class ValidationItemControllerV2 constructor(private val itemRepository: ItemRep
     return "validation/v2/addForm"
   }
 
-  @PostMapping("/add")
+//  @PostMapping("/add")
   fun addItemV1(@ModelAttribute item: Item?, bindingResult: BindingResult, redirectAttributes: RedirectAttributes, model: Model): String {
     checkValidation(item, bindingResult)
+    // 검증에 실패하면 다시 입력 폼으로
+    if (bindingResult.hasErrors()) {
+      logger.error("errors = {}", bindingResult)
+      return "validation/v2/addForm"
+    }
+
+    item?.let {
+      val savedItem = itemRepository.save(item)
+      redirectAttributes.addAttribute("itemId", savedItem.id)
+      redirectAttributes.addAttribute("status", true)
+    }
+
+    return "redirect:/validation/v2/items/{itemId}"
+  }
+
+  @PostMapping("/add")
+  fun addItemV2(@ModelAttribute item: Item?, bindingResult: BindingResult, redirectAttributes: RedirectAttributes, model: Model): String {
+    checkValidation2(item, bindingResult)
     // 검증에 실패하면 다시 입력 폼으로
     if (bindingResult.hasErrors()) {
       logger.error("errors = {}", bindingResult)
@@ -76,6 +94,28 @@ class ValidationItemControllerV2 constructor(private val itemRepository: ItemRep
       // 복합 룰 검증
       if (item.quantity * item.price < 10000) {
         bindingResult.addError(ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = ${item.quantity * item.price}"))
+      }
+    }
+  }
+
+  private fun checkValidation2(item: Item?, bindingResult: BindingResult) {
+    item?.let {
+      // 검증 로직
+      if (!StringUtils.hasText(item.itemName)) {
+        bindingResult.addError(FieldError("item", "itemName", item.itemName, false, null, null, "상품 이름은 필수입니다."))
+      }
+      // price 의 기본값으로 -1 을 세팅했기 때문에 null 체크는 무시
+      if (item.price < 1000 || item.price > 1000000) {
+        bindingResult.addError(FieldError("item", "price", item.price, false, null, null, "가격은 1,000 ~ 1,000,000 까지 허용합니다."))
+      }
+
+      if (item.quantity >= 9999) {
+        bindingResult.addError(FieldError("item", "quantity", item.quantity, false, null, null, "수량은 최대 9,999 까지 허용합니다."))
+      }
+
+      // 복합 룰 검증
+      if (item.quantity * item.price < 10000) {
+        bindingResult.addError(ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = ${item.quantity * item.price}"))
       }
     }
   }
